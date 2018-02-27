45715b9070d4ae2d1948913a5ce2bb87c46ffc70
Garrett Battaglia <garrett@garrettbattaglia.info>
2018-02-27 08:51:08 -0600

    forkk to make webserver

--- src-serv/main.c
+++ src-serv/main.c
@@ -1,7 +1,22 @@
 #include "net.h"
+#include <unistd.h>
+#include <stdio.h>
 
-
-int main(){
-   int socket = openTechServer();
-   techListen(socket);
+int main()
+{
+    pid_t pid = fork();
+    if (pid < 0)
+    {
+        printf("fork error");
+    }
+    else if (pid > 0)
+    {
+        //web server
+        while(1);
+    }
+    else
+    {
+        int socket = openTechServer();
+        techListen(socket);
+    }
 }
